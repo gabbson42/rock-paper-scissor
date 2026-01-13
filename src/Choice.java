0b72889568eb8a38927b28f0d22378887dac561b
Gabriel Landvik Åkerman <gabriel.landvikakerman@gmail.com>
2026-01-13 23:20:08 +0100

    Fixat beats metoden i Choice.

--- src/Choice.java
+++ src/Choice.java
@@ -4,8 +4,11 @@ public enum Choice {
     PAPER,
     SCISSOR;
 
-    public boolean beats(Choice choice){
+    public boolean beats(Choice other){
 
+        if (this == ROCK && other == SCISSOR) return true;
+        if (this == PAPER && other == ROCK) return true;
+        if (this == SCISSOR && other == PAPER) return true;
         return false;
     }
 }
